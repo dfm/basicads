5cf71e26a7278bc391934b7dc2f6170bc7ca9838
Dan F-M <foreman.mackey@gmail.com>
2019-05-20 13:55:57 -0700

    making flask app

--- basicads.py
+++ basicads.py
@@ -6,11 +6,18 @@ from __future__ import division, print_function
 __all__ = []
 
 import re
-import ads
 import shlex
+from operator import itemgetter
 from collections import namedtuple
 
+import ads
 
+import flask
+from werkzeug.contrib.cache import SimpleCache
+
+
+app = flask.Flask(__name__)
+cache = SimpleCache()
 Tokens = namedtuple("Tokens", ("years", "authors"))
 
 
@@ -58,7 +65,63 @@ def perform_query(query):
     # query = ads.ExportQuery(bibcodes=bibcodes, format="bibtex")
     # bibtex = (b for b in query.execute().split("\n\n"))
 
-    return papers
+    dicts = []
+    for paper in papers:
+        aid = [":".join(t.split(":")[1:]) for t in paper.identifier
+               if t.startswith("arXiv:")]
+        for t in paper.identifier:
+            if len(t.split(".")) != 2:
+                continue
+            try:
+                list(map(int, t.split(".")))
+            except ValueError:
+                pass
+            else:
+                aid.append(t)
+        try:
+            page = int(paper.page[0])
+        except (ValueError, TypeError):
+            page = None
+            if paper.page is not None and paper.page[0].startswith("arXiv:"):
+                aid.append(":".join(paper.page[0].split(":")[1:]))
+        dicts.append(dict(
+            doctype=paper.doctype,
+            authors=list(paper.author),
+            year=paper.year,
+            pubdate=paper.pubdate,
+            doi=paper.doi[0] if paper.doi is not None else None,
+            title=paper.title[0],
+            pub=paper.pub,
+            volume=paper.volume,
+            page=page,
+            arxiv=aid[0] if len(aid) else None,
+            citations=(paper.citation_count
+                       if paper.citation_count is not None else 0),
+            url="https://ui.adsabs.harvard.edu/abs/{0}/abstract"
+                .format(paper.bibcode),
+        ))
+    return sorted(dicts, key=itemgetter("pubdate"), reverse=True)
+
+
+@app.route("/search/")
+def search():
+    q = flask.request.args.get("q", None)
+    if q is None:
+        return flask.redirect(flask.url_for("index"))
+
+    # Get the list of papers from the cache if it exists
+    papers = cache.get(q)
+    if papers is None:
+        # Otherwise hit ADS with the query
+        papers = perform_query(q)
+        cache.set(q, papers)
+
+    return flask.render_template("results.html", papers=papers)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
 
 
 if __name__ == "__main__":
